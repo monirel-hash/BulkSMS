f1faf555366e26cc53df6fc30ade8a31f4f7155a
RealMunir <realmunir@RealMunirs-MacBook-Air.local>
2024-08-10 11:45:42 +0100

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # BulkSMS
-# BulkSMS
-# BulkSMS
-# BulkSMS
-# BulkSMS
+
+Sample script for sending iMessage using Python3 and Apple Script.
+
+Usage (MacOS with active iMessage only):
+
+```bash
+python3 send_sms_imessage.py
+```
+
+> I used Python to make it simple xD
+
+Inspired by [Night].
+# BulkSMS# BulkSMS
--- send_sms_imessage.py
+++ send_sms_imessage.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+# Author: RealMunir
+# GitHub: https://github.com/monirel-hash
 
 import os
 import time
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import subprocess
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
 
 # Setup Google Sheets API
 def setup_google_sheets():
@@ -16,10 +21,25 @@ def setup_google_sheets():
 # Function to send SMS
 def send_sms(phone_number, message):
     script_path = 'scripts/sendiMessage.scpt'
+    # Convert phone number to string
+    phone_number = str(phone_number)
+    message = str(message)  # Ensure message is also a string
     command = ['osascript', script_path, phone_number, message]
-    result = subprocess.run(command, capture_output=True, text=True)
-    if result.returncode != 0:
-        print(f"Error sending message: {result.stderr}")
+
+    print(f"Script path: {script_path}")
+    print(f"Phone number: {phone_number}")
+    print(f"Message: {message}")
+    print(f"Command: {command}")
+
+    try:
+        result = subprocess.run(command, capture_output=True, text=True)
+        if result.returncode != 0:
+            print(f"Error sending message: {result.stderr}")
+        else:
+            print(f"Message sent successfully: {result.stdout}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 
 # Main function
 def main():
